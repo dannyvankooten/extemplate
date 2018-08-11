4b8c4651da07085737ce377b2f1577991cbdec28
Danny van Kooten <dannyvankooten@gmail.com>
2018-08-11 09:39:58 +0200

    parse templates correctly, incl. partials. can this be done more efficient?

--- template.go
+++ template.go
@@ -2,7 +2,6 @@ package grender
 
 import (
 	"bufio"
-	"fmt"
 	"html/template"
 	"io/ioutil"
 	"os"
@@ -23,10 +22,10 @@ func init() {
 
 func ParseDir(r string) map[string]*template.Template {
 	templateSet := map[string]*template.Template{}
-	set := template.New("")
+	all := template.New("")
+	files := []string{}
 
-	// replace existing templates.
-	// NOTE: this is unsafe, but Debug should really not be true in production environments.
+	// find all template files
 	err := filepath.Walk(r, func(path string, info os.FileInfo, err error) error {
 		// skip dirs as they can never be valid templates
 		if info == nil || info.IsDir() {
@@ -39,14 +38,30 @@ func ParseDir(r string) map[string]*template.Template {
 			return nil
 		}
 
-		fmt.Printf("\n")
-		fmt.Printf("Processing %s\n", path)
+		files = append(files, path)
+		return nil
+	})
 
+	if err != nil {
+		// TODO: Handle error
+	}
+
+	// parse all templates into a single template (without inheritance)
+	for _, f := range files {
+		b, err := ioutil.ReadFile(f)
+		if err != nil {
+			// TODO: handle error
+		}
+		all.Parse(string(b))
+	}
+
+	// then, parse all templates again but with inheritance
+	for _, path := range files {
 		// get template name: root/users/detail.html => users/detail.html
 		name := strings.TrimPrefix(path, r)
 		layout := getLayoutForTemplate(path)
-		tmpl := template.New(name)
-
+		//tmpl := template.New(name)
+		tmpl := template.Must(all.Clone()).New(name)
 		//tmpl := template.Must(baseTmpl.Clone()).New(name)
 
 		templateFiles := []string{path}
@@ -54,32 +69,19 @@ func ParseDir(r string) map[string]*template.Template {
 		if layout != "" {
 			layoutFile := filepath.Join(filepath.Dir(path), layout)
 			templateFiles = append(templateFiles, layoutFile)
-			// todo: keep looking for template files
+			// todo: keep looking for layout files
 		}
-
-		fmt.Printf("Templates for %s: %#v\n", name, templateFiles)
 
 		// parse templates in reverse order
 		// this is important because we need templates defined in child files to override templates defined in parent files
-		//for i, j := 0, len(templateFiles); i < j; i, j = i+1, j-1 {
-		//for j := len(templateFiles) - 1; j >= 0; j-- {
-		for j, _ := range templateFiles {
-			fmt.Printf("Parsing %s\n", templateFiles[j])
+		for j := len(templateFiles) - 1; j >= 0; j-- {
+			//for j, _ := range templateFiles {
 			b, _ := ioutil.ReadFile(templateFiles[j])
 			tmpl.Parse(string(b))
 		}
 
-		//tmpl = template.Must(tmpl.ParseFiles(templateFiles...))
-		fmt.Printf("Template for %s: %#v\n", path, tmpl.DefinedTemplates())
-
 		templateSet[name] = tmpl
-		return nil
-	})
-	if err != nil {
-		// TODO: Handle error
 	}
-
-	fmt.Printf("%#v", set)
 
 	return templateSet
 }
--- template_test.go
+++ template_test.go
@@ -2,7 +2,7 @@ package grender
 
 import (
 	"bytes"
-	"fmt"
+	"strings"
 	"testing"
 )
 
@@ -13,15 +13,29 @@ func TestParseDir(t *testing.T) {
 func TestTemplate(t *testing.T) {
 	tmpl := ParseDir("examples/")
 
-	if _, ok := tmpl["child.tmpl"]; !ok {
-		t.Errorf("template not found")
-	} else {
-		var buf bytes.Buffer
-		if err := tmpl["child.tmpl"].Execute(&buf, nil); err != nil {
-			t.Errorf("error executing child.tmpl: %s", err)
-		}
-		fmt.Printf("Executed template %s: %s\n", "child.tmpl", string(buf.Bytes()))
+	tests := map[string]string{
+		"hello.tmpl":              "Hello !",                               // normal template, no inheritance
+		"child.tmpl":              "Hello world from the master template.", // template with inheritance
+		"master.tmpl":             "Bye world from the master template.",   // normal template with {{ block }}
+		"child-with-partial.tmpl": "Hello world! How are we today? from the master template.",
 	}
+
+	for k, v := range tests {
+		if _, ok := tmpl[k]; !ok {
+			t.Errorf("template not found in set: %s", k)
+		}
+
+		var buf bytes.Buffer
+		if err := tmpl[k].Execute(&buf, nil); err != nil {
+			t.Errorf("error executing template %s: %s", k, err)
+		}
+
+		e := strings.TrimSpace(buf.String())
+		if e != v {
+			t.Errorf("incorrect template result. \nExpected: %s\nActual: %s", v, e)
+		}
+	}
+
 }
 
 func BenchmarkGrenderGetLayoutForFile(b *testing.B) {
