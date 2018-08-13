119c195db278d2bd31dfa7a76b8777b42470c522
Danny van Kooten <dannyvankooten@gmail.com>
2018-08-13 15:29:48 +0200

    add example

---
+++ example_test.go
@@ -0,0 +1,18 @@
+package extemplate_test
+
+import (
+	"html/template"
+	"os"
+	"strings"
+
+	"github.com/dannyvankooten/extemplate"
+)
+
+func ExampleExtemplate_ParseDir() {
+	xt := extemplate.New().Funcs(template.FuncMap{
+		"foo": strings.ToLower,
+	})
+	_ = xt.ParseDir("examples/", []string{".tmpl"})
+	_ = xt.ExecuteTemplate(os.Stdout, "child.tmpl", nil)
+	// Output: Hello from child.tmpl
+}
